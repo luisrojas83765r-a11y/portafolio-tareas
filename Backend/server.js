2bc1c6706fbae1d92432167d8edd7e9cd02f4cb9
Luis Humberto Rojas Montoya <luisrojas83765.r@gmail.com>
2026-06-18 17:24:22 -0700

    Agregar path para frontend

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
 
 app.use(cors());
